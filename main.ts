2b9d0f3e036057e5e3e2957f65c9c5eb75a4e1f3
yene27 <yene27@yahoo.com>
2022-09-01 11:49:08 -0500

    Agregué siempre

--- main.ts
+++ main.ts
@@ -1,13 +1,7 @@
-game.startCountdown(10000)
 let coin = game.createSprite(randint(0, 4), randint(0, 4))
 let char = game.createSprite(2, 2)
 coin.set(LedSpriteProperty.Brightness, 150)
-while (true) {
-    if (coin.isTouching(char)) {
-        game.addScore(1)
-        coin.set(LedSpriteProperty.X, randint(0, 4))
-        coin.set(LedSpriteProperty.Y, randint(0, 4))
-    }
+basic.forever(function () {
     if (input.acceleration(Dimension.X) > 0) {
         char.change(LedSpriteProperty.X, 1)
         basic.pause(100)
@@ -24,4 +18,8 @@ while (true) {
         char.change(LedSpriteProperty.Y, -1)
         basic.pause(100)
     }
-}
+    if (coin.isTouching(char)) {
+        coin.set(LedSpriteProperty.X, randint(0, 4))
+        coin.set(LedSpriteProperty.Y, randint(0, 4))
+    }
+})
