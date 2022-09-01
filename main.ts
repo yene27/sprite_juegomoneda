8b422040a6ba43eac020c1f8ac1011bd5019d3fa
yene27 <yene27@yahoo.com>
2022-09-01 11:53:02 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
+function introJuego () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(200)
+    basic.clearScreen()
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
+}
+introJuego()
 let coin = game.createSprite(randint(0, 4), randint(0, 4))
 let char = game.createSprite(2, 2)
 coin.set(LedSpriteProperty.Brightness, 150)
+game.setScore(0)
 basic.forever(function () {
     if (input.acceleration(Dimension.X) > 0) {
         char.change(LedSpriteProperty.X, 1)
@@ -19,6 +29,7 @@ basic.forever(function () {
         basic.pause(100)
     }
     if (coin.isTouching(char)) {
+        game.setScore(game.score() + 1)
         coin.set(LedSpriteProperty.X, randint(0, 4))
         coin.set(LedSpriteProperty.Y, randint(0, 4))
     }
